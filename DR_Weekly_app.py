b93f02bc267685fba074df3785efe6f83ce65816
MetalRoosterSimulation <flopax29@gmail.com>
2026-03-09 17:05:58 -0400

    broken: remove xls html fallback for demo

--- DR_Weekly_app.py
+++ DR_Weekly_app.py
@@ -43,9 +43,6 @@ def process():
         if filename.endswith(".xls"):
             try:
                 df = pd.read_excel(file, engine="xlrd")
-            except Exception:
-                file.seek(0)
-                df = pd.read_html(file, header=0)[0]
         elif filename.endswith(".xlsx"):
             df = pd.read_excel(file, engine="openpyxl")
         elif filename.endswith(".csv"):
